c99c4df57a2a8c0e18aa69f8e7078bf621bf7a42
David Muhanguzi <muhunksdavie95@gmail.com>
2019-03-22 17:00:28 +0300

    add crud for bucketlist items

--- helloapp/api/tests.py
+++ helloapp/api/tests.py
@@ -37,3 +37,36 @@ class ViewTestCase(TestCase):
         """Test the api has bucket creation capability."""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
 
+    def test_api_can_get_a_bucketlist(self):
+        """Test that the api can get a given bucketlist"""
+        bucketlist = Bucketlist.objects.get()
+        response  = self.client.get(
+            reverse(
+                'details',
+                kwargs={'pk': bucketlist.id}
+            ),
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, bucketlist)
+
+    def test_api_can_update_a_bucketlist(self):
+        """Test the api can update a given bucketlist."""
+        bucketlist = Bucketlist.objects.get()
+        change_bucketlist = {'name': 'something new'}
+        res = self.client.put(
+            reverse('details', kwargs={'pk': bucketlist.id}),
+            change_bucketlist,
+            format='json'
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_bucketlist(self):
+        """Test the api can delete a bucketlist."""
+        bucketlist = Bucketlist.objects.get()
+        response = self.client.delete(
+            reverse('details', kwargs={'pk': bucketlist.id}),
+            format='json',
+            follow=True
+        ) 
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
--- helloapp/api/views.py
+++ helloapp/api/views.py
@@ -13,3 +13,8 @@ class CreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         """Save post data when creating a new bucketlist."""
         serializer.save()
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """This class handles the http GET, PUT and DELETE requests."""
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
